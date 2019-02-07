952001ace63ff9224bd004072aad78a2ad720486
said shah <saidmysr20@aol.com>
2019-02-06 20:26:26 -0500

    created demo and animal classes

---
+++ Animal.java
@@ -0,0 +1,33 @@
+public class Animal{
+
+  public static int animalId;
+  private String name;
+  private int age;
+
+  public Animal(String name, int age){
+    this.name = name;
+    this.age = age;
+    this.animalId++;
+  }
+
+  public String getName(){
+    return this.name;
+  }
+
+  public void setName(String name){
+    this.name = name;
+  }
+
+  public int getAge(){
+    return this.age;
+  }
+
+  public void setAge(int age){
+    this.age = age;
+  }
+
+  public String toString(){
+    return "My name is "+ this.name + " and my age is "+this.age;
+  }
+
+}
---
+++ Demo.java
@@ -0,0 +1,15 @@
+
+public class Demo{
+
+public static void main(String[] args) {
+
+ Animal animal1 = new Animal("john",22);
+ Animal animal2 = new Animal("mike",32);
+ Animal animal3 = new Animal("rex",13);
+ System.out.println(animal1.toString());
+ System.out.println(animal2.toString());
+ System.out.println(animal3.toString());
+ System.out.println(Animal.animalId);
+}
+
+}
